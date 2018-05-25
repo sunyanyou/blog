da23decdc1aa8c339a2b2693906e6dde4f7621c2
sunyanyou <sunyanyou123@163.com>
2018-05-25 23:46:56 +0800

    login and logout

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -5,18 +5,52 @@ use app\admin\model\Admin;
 use houdunwang\crypt\Crypt;
 use think\controller;
 use think\Request;
+use think\session;
 
-class Login extends controller {
-	public function index(){
-
-        //开始判断登录
-		if(request()->ispost()){
-		    $res = (new Admin())->login(input('post.'));
-            if($res['valid']){
+//class Login extends Controller {
+//	public function index(){
+//
+//        //开始判断登录
+//		if(request()->ispost()){
+//		    $res = (new Admin())->login(input('post.'));
+//            if($res['valid']){
+//                $this->success($res['msg'],'admin/index/index');
+//            }
+//		}
+//		return $this->fetch();
+//	}
+//    //退出登录
+//    public function logout(){
+//	    session::clear();
+//	    $this->success('退出登录','admin/login/index');
+//    }
+//}
+class Login extends Controller
+{
+    public function index()
+    {
+        //echo Crypt::encrypt('admin888');//加密 h3vPU8JGuF3VS/uxIpjRSw==
+        //echo Crypt::decrypt('h3vPU8JGuF3VS/uxIpjRSw==');//测试解密  admin888
+        //测试数据库连接
+        //$data = db('admin')->find(1);
+        //dump($data);
+        if(request()->isPost()){
+            $res = (new Admin())->login(input('post.'));
+            if($res['valid'])
+            {
+                //说明登录成功
                 $this->success($res['msg'],'admin/index/index');
+            }else{
+                //说明登录失败
+                $this->error($res['msg']);
             }
-		}
-		return $this->fetch();
-	}
-
+        }
+        //加载我们登录页面
+        return $this->fetch('index');
+    }
+    //退出登录
+    public function logout(){
+        Session::clear();
+        $this->success('退出登录','admin/login/index');
+    }
 }
--- application/admin/model/Admin.php
+++ application/admin/model/Admin.php
@@ -12,27 +12,49 @@ class Admin extends Model
     //设置当前模型对应的完整数据表名称
     protected $table = 'blog_admin';
     //开始登录
-    public function login($data){
-        //进行验证用户名、密码和验证码是否为空
+//    public function login($data){
+//        //进行验证用户名、密码和验证码是否为空
+//        $validate = Loader::validate('Admin');
+//        //如果为空
+//        if(!$validate->check($data)){
+//            return ['valid'=>0,'msg'=>$validate->getError()];
+//        }
+//        //比对用户名和密码是否和数据库中的相同
+//        $userInfo = $this->where('admin_username',$data['admin_username'])->where('admin_password',Crypt::encrypt
+//        ($data['admin_password']))->find();
+//
+//
+//        //如果与数据库中的数据不匹配
+//        if(!$userInfo){
+//            return ['valid'=>0,'msg'=>'用户名或者密码不正确'];
+//        }
+//        //将用户信息存入到session中
+//        session('admin.admin_id',$userInfo['admin_id']);
+//        session('admin.admin_username',$userInfo['admin_username']);
+//        return ['valid'=>1,'msg'=>'登录成功'];
+//
+//    }
+
+    public function login($data)
+    {
+        //1.执行验证
         $validate = Loader::validate('Admin');
-        //如果为空
+        //如果验证不通过
         if(!$validate->check($data)){
             return ['valid'=>0,'msg'=>$validate->getError()];
         }
-        //比对用户名和密码是否和数据库中的相同
-        $userInfo = $this->where('admin_username',$data['admin_username'])->where('admin_password',Crypt::encrypt
-        ($data['admin_password']))->find();
-
-
-        //如果与数据库中的数据不匹配
-        if(!$userInfo){
+        //2.比对用户名和密码是否正确
+        $userInfo = $this->where('admin_username',$data['admin_username'])->where('admin_password',Crypt::encrypt($data['admin_password']))->find();
+        //halt($userInfo);
+        if(!$userInfo)
+        {
+            //说明在数据库未匹配到相关数据
             return ['valid'=>0,'msg'=>'用户名或者密码不正确'];
         }
-        //将用户信息存入到session中
+        //3.将用户信息存入到session中
         session('admin.admin_id',$userInfo['admin_id']);
         session('admin.admin_username',$userInfo['admin_username']);
         return ['valid'=>1,'msg'=>'登录成功'];
-
     }
 
 }
