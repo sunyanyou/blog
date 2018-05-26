75192d02c80b567b02ba68fa17ef3ae01afd395b
sunyanyou <sunyanyou123@163.com>
2018-05-26 16:56:54 +0800

    delete

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,8 +3,10 @@ namespace app\admin\controller;
 
 class Index extends Common
 {
+    //后台首页
     public function index()
     {
         return $this->fetch();
     }
+
 }
--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -29,11 +29,7 @@ class Login extends Controller
 {
     public function index()
     {
-        //echo Crypt::encrypt('admin888');//加密 h3vPU8JGuF3VS/uxIpjRSw==
-        //echo Crypt::decrypt('h3vPU8JGuF3VS/uxIpjRSw==');//测试解密  admin888
-        //测试数据库连接
-        //$data = db('admin')->find(1);
-        //dump($data);
+
         if(request()->isPost()){
             $res = (new Admin())->login(input('post.'));
             if($res['valid'])
