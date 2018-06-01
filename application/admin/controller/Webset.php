fad6f587379d920c361235e54e09cce5e0a0e04a
sunyanyou <sunyanyou123@163.com>
2018-06-01 22:00:38 +0800

    webset

--- application/admin/controller/Webset.php
+++ application/admin/controller/Webset.php
@@ -8,6 +8,20 @@ class Webset extends Common
 {
     //网站设置首页
     public function index(){
+        $list = db('webset')->select();
+        $this->assign('list',$list);
         return $this->fetch();
+    }
+    //修改网站设置
+
+    public function edit(){
+        if(request()->isAjax()){
+            $res = (new \app\admin\model\Webset())->edit(input('post.'));
+            if($res['valid']){
+                $this->success($res['msg'],'index');
+            }else{
+                $this->error($res['msg']);
+            }
+        }
     }
 }
